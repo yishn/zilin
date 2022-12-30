a4c81ded5afc5a37102b427df03c08136a1f529b
Yichuan Shen <shen.yichuan@gmail.com>
2022-12-30 22:21:49 +0100

    Automatically download character dictionary

--- ci/updateData.ts
+++ ci/updateData.ts
@@ -2,19 +2,43 @@ import { Foras, gunzip } from "https://deno.land/x/foras@2.0.2/deno/mod.ts";
 
 await Foras.initBundledOnce();
 
-console.log("Downloading CEDICT...");
+async function downloadCedict() {
+  console.log("Downloading CEDICT...");
 
-const response = await fetch(
-  "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz"
-);
+  const response = await fetch(
+    "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz"
+  );
 
-if (!response.ok) {
-  throw new Error("Downloading CEDICT failed.");
+  if (!response.ok) {
+    throw new Error("Downloading CEDICT failed.");
+  }
+
+  const data = gunzip(new Uint8Array(await response.arrayBuffer()));
+
+  await Deno.writeFile(
+    new URL("../data/cedict_1_0_ts_utf-8_mdbg.txt", import.meta.url),
+    data
+  );
 }
 
-const data = gunzip(new Uint8Array(await response.arrayBuffer()));
+async function downloadCharacterDictionary() {
+  console.log("Downloading dictionary...");
 
-await Deno.writeFile(
-  new URL("../data/cedict_1_0_ts_utf-8_mdbg.txt", import.meta.url),
-  data
-);
+  const response = await fetch(
+    "https://github.com/skishore/makemeahanzi/raw/master/dictionary.txt"
+  );
+
+  if (!response.ok) {
+    throw new Error("Downloading character dictionary failed.");
+  }
+
+  const data = new Uint8Array(await response.arrayBuffer());
+
+  await Deno.writeFile(
+    new URL("../data/dictionary.txt", import.meta.url),
+    data
+  );
+}
+
+await downloadCedict();
+await downloadCharacterDictionary();
